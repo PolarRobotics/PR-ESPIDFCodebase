a92525df6313aacd324051b7c11cb5815dbaf6fe
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-11-20 20:43:17 +0000

    IT WORKS!! IT WORKS!! IT WORKS!! IT WORKS!!! IT WORKS!! IT WORKS!! IT WORKS!!!!!!!!!!!!!!!

--- main/main_robot.cpp
+++ main/main_robot.cpp
@@ -248,7 +248,7 @@ extern "C" void main_app(void)
         //* Update the motors based on the inputs from the controller
         //* Can change functionality depending on subclass, like robot.action()
         drive->update();
-        // drive->printDebugInfo(); // comment this line out to reduce compile time and memory usage
+        drive->printDebugInfo(); // comment this line out to reduce compile time and memory usage
         // drive->printCsvInfo(); // prints info to serial monitor in a csv (comma separated value) format
       }
       //! Performs all special robot actions depending on the instantiated Robot subclass
