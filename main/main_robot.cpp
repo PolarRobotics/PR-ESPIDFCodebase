b0ab00c3d69fd897a64d14924fceb79db3e6dee0
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2026-09-09 20:30:31 -0400

    main robot reformat

--- main/main_robot.cpp
+++ main/main_robot.cpp
@@ -3,12 +3,14 @@
  *
  * The place where the magic happens.
  * This code is what's run by default by the ESP32.
- * `setup()` is called once upon startup. Initialization and preparation occur here.
- * `loop()` is called infinitely after `setup()` completes. Most real-time logic is here.
+ * `setup()` is called once upon startup. Initialization and preparation occur
+ *here. `loop()` is called infinitely after `setup()` completes. Most real-time
+ *logic is here.
  **/
 
 #include <Arduino.h>
-#include <ps5Controller.h> // ESP PS5 library, access using global instance `ps5`
+#include <ps5Controller.h>  // ESP PS5 library, access using global instance `ps5`
+
 #include "esp_log.h"
 
 static const char *TAG = "MainRobot";
@@ -32,29 +34,29 @@ static const char *TAG = "MainRobot";
 #include <pairing.h>
 
 // Robot Includes
-#include <Robot.h>
-#include <Lineman.h>
 #include <Center.h>
 #include <CenterConversion.h>
 #include <Kicker.h>
+#include <Lineman.h>
 #include <Quarterback.h>
 #include <QuarterbackBase.h>
 #include <QuarterbackTurret.h>
+#include <Robot.h>
 
 // Types Includes
 #include <BotTypes.h>
 
 // Utilities Includes
 #include <ConfigManager.h>
-#include <TackleSensor.h>
 #include <PRConsole.h>
+#include <TackleSensor.h>
 
 // Sabertooth USB Serial Library
 #include <sabertoothinst.h>
 
 // Primary Parent Component Pointers
-Robot *robot = nullptr; // subclassed if needed
-Drive *drive = nullptr; // subclassed if needed
+Robot *robot = nullptr;  // subclassed if needed
+Drive *drive = nullptr;  // subclassed if needed
 
 //* How to use subclasses: ((SubclassName*) robot)->function()
 //! You must downcast each time you use a special function
@@ -81,13 +83,9 @@ void onConnection()
 
   // TODO: perm sln
   if (robotType != quarterback_turret)
-  {
     drive->emergencyStop();
-  }
   else
-  {
     ((QuarterbackTurret *)robot)->emergencyStop();
-  }
 }
 
 void onDisconnect()
@@ -96,13 +94,9 @@ void onDisconnect()
 
   // TODO: perm sln
   if (robotType != quarterback_turret)
-  {
     drive->emergencyStop();
-  }
   else
-  {
     ((QuarterbackTurret *)robot)->emergencyStop();
-  }
 }
 
 extern "C" void main_app(void)
@@ -122,8 +116,8 @@ extern "C" void main_app(void)
 
   // Arduino-like setup()
   pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(TACKLE_PIN, OUTPUT); // Try INPUT_PULLUP
-  digitalWrite(TACKLE_PIN, 0); // Initially sets tackle sensor to home
+  pinMode(TACKLE_PIN, OUTPUT);  // Try INPUT_PULLUP
+  digitalWrite(TACKLE_PIN, 0);  // Initially sets tackle sensor to home
 
   // Initialize debouncer
   dbOptions = new Debouncer(TACKLE_SENSOR_DELAY);
@@ -139,75 +133,77 @@ extern "C" void main_app(void)
   // Drive motor identifiers:
   // - Packet Serial robots: use Sabertooth motor indices (M1_IDX/M2_IDX)
   // - PWM robots: use GPIO pins (M1_PWM/M2_PWM)
-  const bool driveUsesPwmPins = (MOTORTYPE_INTERFACE_ARRAY[driveParams.motor_type] == pwm);
+  const bool driveUsesPwmPins =
+      (MOTORTYPE_INTERFACE_ARRAY[driveParams.motor_type] == pwm);
   const uint8_t DRIVE_M1 = driveUsesPwmPins ? M1_PWM : M1_IDX;
   const uint8_t DRIVE_M2 = driveUsesPwmPins ? M2_PWM : M2_IDX;
 
-  // work backwards from highest ordinal enum since lineman should be default case
+  // work backwards from highest ordinal enum since lineman should be default
+  // case
   switch (robotType)
   {
-  //* Each case should have the following:
-  // An initialization of `robot` as a new Robot subclass
-  // An initialization of `drive` as a new Drive subclass
-  // A call to drive->setupMotors (or downcast and call to override)
-  case kicker:
-    robot = new Kicker(SPECBOT_PIN1, SPECBOT_PIN2, ENC1_CHA, ENC1_CHB);
-    drive = new Drive(kicker, driveParams);
-    drive->setupMotors(DRIVE_M1, DRIVE_M2);
-    break;
-  case quarterback_old:
-    robot = new Quarterback(SPECBOT_PIN1, SPECBOT_PIN2, SPECBOT_PIN3);
-    drive = new Drive(quarterback_old, driveParams);
-    drive->setupMotors(DRIVE_M1, DRIVE_M2);
-    break;
-  case center:
-    robot = new Center(SPECBOT_PIN1, SPECBOT_PIN2);
-    drive = new Drive(center, driveParams);
-    drive->setupMotors(DRIVE_M1, DRIVE_M2);
-    break;
-  case center_conversion:
-    robot = new CenterConversion(SPECBOT_PIN1);
-    drive = new Drive(center_conversion, driveParams);
-    drive->setupMotors(DRIVE_M1, DRIVE_M2);
-    break;
-  case runningback:
-    robot = new Lineman();
-    drive = new Drive(runningback, driveParams);
-    drive->setupMotors(DRIVE_M1, DRIVE_M2);
-    break;
-  case quarterback_turret:
-    robot = new QuarterbackTurret(
-        M1_IDX,       // left flywheel
-        M2_IDX,       // right flywheel
-        M3_PIN,       // cradle
-        M4_PIN,       // turret
-        SPECBOT_PIN1, // assembly motor
-        SPECBOT_PIN3, // magnetometer sda
-        SPECBOT_PIN4, // magnetometer scl
-        ENC1_CHA,     // turret encoder
-        ENC1_CHB,     // turret encoder
-        ENC2_CHB      // zeroing laser
-    );
-    break;
-  case quarterback_base:
-    drive = new Drive(quarterback_base, driveParams);
-    drive->setupMotors(DRIVE_M1, DRIVE_M2);
-    robot = new QuarterbackBase(drive);
-    break;
-  case receiver:
-  case lineman:
-  default: // Assume lineman
-    robot = new Lineman();
-    {
-      String debugMsg = "01: Instantiating Drive Class\n";
-      ESP_LOGI(TAG, "%s", debugMsg.c_str());
-    }
-    drive = new Drive(lineman, driveParams);
-    {
-      String debugMsg2 = "03: Call setupMotors\n";
-      ESP_LOGI(TAG, "%s", debugMsg2.c_str());
-    }
-    drive->setupMotors(DRIVE_M1, DRIVE_M2);
+    //* Each case should have the following:
+    // An initialization of `robot` as a new Robot subclass
+    // An initialization of `drive` as a new Drive subclass
+    // A call to drive->setupMotors (or downcast and call to override)
+    case kicker:
+      robot = new Kicker(SPECBOT_PIN1, SPECBOT_PIN2, ENC1_CHA, ENC1_CHB);
+      drive = new Drive(kicker, driveParams);
+      drive->setupMotors(DRIVE_M1, DRIVE_M2);
+      break;
+    case quarterback_old:
+      robot = new Quarterback(SPECBOT_PIN1, SPECBOT_PIN2, SPECBOT_PIN3);
+      drive = new Drive(quarterback_old, driveParams);
+      drive->setupMotors(DRIVE_M1, DRIVE_M2);
+      break;
+    case center:
+      robot = new Center(SPECBOT_PIN1, SPECBOT_PIN2);
+      drive = new Drive(center, driveParams);
+      drive->setupMotors(DRIVE_M1, DRIVE_M2);
+      break;
+    case center_conversion:
+      robot = new CenterConversion(SPECBOT_PIN1);
+      drive = new Drive(center_conversion, driveParams);
+      drive->setupMotors(DRIVE_M1, DRIVE_M2);
+      break;
+    case runningback:
+      robot = new Lineman();
+      drive = new Drive(runningback, driveParams);
+      drive->setupMotors(DRIVE_M1, DRIVE_M2);
+      break;
+    case quarterback_turret:
+      robot = new QuarterbackTurret(
+        M1_IDX,        // left flywheel
+        M2_IDX,        // right flywheel
+        M3_PIN,        // cradle
+        M4_PIN,        // turret
+        SPECBOT_PIN1,  // assembly motor
+        SPECBOT_PIN3,  // magnetometer sda
+        SPECBOT_PIN4,  // magnetometer scl
+        ENC1_CHA,      // turret encoder
+        ENC1_CHB,      // turret encoder
+        ENC2_CHB       // zeroing laser
+      );
+      break;
+    case quarterback_base:
+      drive = new Drive(quarterback_base, driveParams);
+      drive->setupMotors(DRIVE_M1, DRIVE_M2);
+      robot = new QuarterbackBase(drive);
+      break;
+    case receiver:
+    case lineman:
+    default:  // Assume lineman
+      robot = new Lineman();
+      {
+        String debugMsg = "01: Instantiating Drive Class\n";
+        ESP_LOGI(TAG, "%s", debugMsg.c_str());
+      }
+      drive = new Drive(lineman, driveParams);
+      {
+        String debugMsg2 = "03: Call setupMotors\n";
+        ESP_LOGI(TAG, "%s", debugMsg2.c_str());
+      }
+      drive->setupMotors(DRIVE_M1, DRIVE_M2);
   }
 
   drive->printSetup();
@@ -217,9 +213,10 @@ extern "C" void main_app(void)
 
   ps5.attachOnConnect(onConnection);
   ps5.attachOnDisconnect(onDisconnect);
-  HWSerial.begin(115200, SERIAL_8N1, 16, 17); // 9600 baudrate default for USBSabertooth
+  HWSerial.begin(115200, SERIAL_8N1, 16,
+                 17);  // 9600 baudrate default for USBSabertooth
   {
-    ; // wait for serial port to connect
+    ;  // wait for serial port to connect
   }
 
   /*
@@ -231,18 +228,19 @@ extern "C" void main_app(void)
 
   */
 
-  // runs continuously after Arduino-like setup. controls driving and any special robot functionality during a game
+  // runs continuously after Arduino-like setup. controls driving and any
+  // special robot functionality during a game
 
   // Arduino-like loop()
   while (true)
   {
-
     if (ps5.isConnected())
     {
       // ESP_LOGI(TAG, "\r\nConnected");
       // ps5.setLed(255, 0, 0);   // set LED red
 
-      //* QBv3 Turret doesn't have drive, so this is a temporary measure to avoid NPEs and chaos
+      //* QBv3 Turret doesn't have drive, so this is a temporary measure to
+      // avoid NPEs and chaos
       // TODO: find better solution
       if (robotType != quarterback_turret)
       {
@@ -274,26 +272,26 @@ extern "C" void main_app(void)
         }
 
         // Manual Home / Away Position Setting
-        if (dbOptions->debounceAndPressed(ps5.Options()))
-        {
-          switchTackleSensor();
-        }
+        if (dbOptions->debounceAndPressed(ps5.Options())) switchTackleSensor();
 
         //* Update the motors based on the inputs from the controller
         //* Can change functionality depending on subclass, like robot.action()
         drive->update();
-        // drive->printDebugInfo(); // comment this line out to reduce compile time and memory usage
-        // drive->printCsvInfo(); // prints info to serial monitor in a csv (comma separated value) format
+        // drive->printDebugInfo(); // comment this line out to reduce compile
+        // time and memory usage drive->printCsvInfo(); // prints info to serial
+        // monitor in a csv (comma separated value) format
       }
-      //! Performs all special robot actions depending on the instantiated Robot subclass
+      //! Performs all special robot actions depending on the instantiated Robot
+      //! subclass
       robot->action();
 
       // DEBUGGING:
-      // drive->printDebugInfo(); // comment this line out to reduce compile time and memory usage
-      // drive->printCsvInfo(); // prints info to serial monitor in a csv (comma separated value) format
+      // drive->printDebugInfo(); // comment this line out to reduce compile
+      // time and memory usage drive->printCsvInfo(); // prints info to serial
+      // monitor in a csv (comma separated value) format
     }
     else
-    { // no response from PS5 controller within last 300 ms, so stop
+    {  // no response from PS5 controller within last 300 ms, so stop
       // ESP_LOGI(TAG, "Controller DC\n");
       if (robotType != quarterback_turret)
       {
@@ -314,8 +312,10 @@ extern "C" void main_app(void)
 
   /**
    * @brief onDisconnect: Function to be called on controller disconnect
-   * Stops bots from driving off and ramming into a wall or someone's foot if they disconnect
+   * Stops bots from driving off and ramming into a wall or someone's foot if
+   * they disconnect
    */
 
-  // WARNING: if program reaches end of function app_main() the MCU will restart.
+  // WARNING: if program reaches end of function app_main() the MCU will
+  // restart.
 }
